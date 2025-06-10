b9032ada56a55f20b363ee3e64416e73a41aee0a
sagarika-padmanaban <sagarikapd@gmail.com>
2025-06-10 17:32:39 +0530

    search fix for lang

--- backend/utils/search.py
+++ backend/utils/search.py
@@ -69,7 +69,10 @@ def process_search_query(
                         parsed_value  # Unaccent doesn't work as intended.
                     )
                 else:
-                    queryset_dict[f"{search_field_name}__{i}"] = parsed_value
+                    if i == 'meta_info_language': 
+                        queryset_dict[f"{search_field_name}__{i}"] = str(parsed_value)
+                    else:
+                        queryset_dict[f"{search_field_name}__{i}"] = parsed_value
             else:
                 if type(parsed_value) != str:
                     queryset_dict[i] = parse_for_data_types(j)
@@ -77,8 +80,6 @@ def process_search_query(
                     queryset_dict[f"{i}__icontains"] = (
                         parsed_value  # Unaccent is not supported for CharField
                     )
-            if i == 'meta_info_language': 
-                queryset_dict[f"{search_field_name}__{i}"] = str(parsed_value)
     except Exception as e:
         print(f"\033[1mError found while processing query dictionary. In: {e}\033[0m")
 
