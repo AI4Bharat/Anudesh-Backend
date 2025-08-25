b730142e4d28cc557d1383c4a7ba35f07462d084
sagarika-padmanaban <sagarikapd@gmail.com>
2025-08-25 09:42:39 +0530

    fix

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1985,6 +1985,29 @@ class ProjectViewSet(viewsets.ModelViewSet):
                 {"message": "This project is not yet published"},
                 status=status.HTTP_403_FORBIDDEN,
             )
+        if cur_user.guest_user:
+            print(f"user{project.metadata_json}")
+            auto_assign_count = project.metadata_json.get('auto_assign_count', 0)
+
+        
+        # Check if guest already has auto-assigned tasks
+            if auto_assign_count > 0:
+                assigned_tasks_count = Task.objects.filter(
+                    project_id=pk,
+                    annotation_users=cur_user.id
+                ).count()
+                print(f"user{assigned_tasks_count}{auto_assign_count}")
+                if assigned_tasks_count >= auto_assign_count:
+                    return Response(
+                        {
+                            "message": "Your tasks have been auto-assigned",
+                        },
+                        status=status.HTTP_200_OK,
+                    )
+                else:
+                    # Set the number of tasks to be assigned
+                    request.data["num_tasks"] = auto_assign_count - assigned_tasks_count
+
         serializer = ProjectUsersSerializer(project, many=False)
         annotators = serializer.data["annotators"]
         annotator_ids = set()
@@ -2166,6 +2189,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
         return Response(
             {"message": "Tasks assigned successfully"}, status=status.HTTP_200_OK
         )
+
     @action(
         detail=False,
         methods=["POST"],
