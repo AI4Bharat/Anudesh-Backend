399bbf9113252705a54ff19d0fd1092c7d79663b
Muhammad Umaid Rather <107248121+humiiii@users.noreply.github.com>
2025-09-05 19:39:11 +0530

    added basic bookmark

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -8,4 +8,7 @@ router.register(r"", ProjectViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('user-projects/', UserProjectListView.as_view(), name='user-projects'),
+    path('bookmark/', BookmarkProjectView.as_view(), name='bookmark-project'),
+    path('unbookmark/', UnbookmarkProjectView.as_view(), name='unbookmark-project'),
 ]
