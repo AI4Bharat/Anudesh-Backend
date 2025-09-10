e0627b16da60431ad4d38ffc09bab1a71cee946c
Ishvinder Sethi <ishvindersethi22@gmail.com>
2025-09-10 15:15:43 +0530

    Revert "Update views.py"

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -2004,6 +2004,10 @@ class ProjectViewSet(viewsets.ModelViewSet):
                         },
                         status=status.HTTP_200_OK,
                     )
+                else:
+                    # Set the number of tasks to be assigned
+                    request.data["num_tasks"] = auto_assign_count - assigned_tasks_count
+
         serializer = ProjectUsersSerializer(project, many=False)
         annotators = serializer.data["annotators"]
         annotator_ids = set()
