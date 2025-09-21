9cd6bfe328dff79927aafb8d070379329d2951a4
munishmangla98 <munishmangla98@gmail.com>
2025-09-21 10:57:09 +0530

    check

--- backend/anudesh_backend/settings.py
+++ backend/anudesh_backend/settings.py
@@ -38,6 +38,8 @@ if DEBUG:
     ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "*"]
 else:
     ALLOWED_HOSTS = [
+        "127.0.0.1", 
+        "localhost",
         "dev.anudesh.ai4bharat.org",
         "0.0.0.0",
         "backend.dev.anudesh.ai4bharat.org",
