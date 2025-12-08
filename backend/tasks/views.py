91a068b455119cfd48d2352def9eb8fc2c3d7c7e
sumitbhukal0007 <sumitbhukal007@gmail.com>
2025-12-08 09:57:33 +0530

    sending updated_at value to frontend

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -380,6 +380,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                             task_obj["id"] = an.task_id
                             task_obj["annotation_status"] = an.annotation_status
                             task_obj["user_mail"] = an.completed_by.email
+                            if("unlabeled" not in ann_status):
+                                task_obj["updated_at"] = utc_to_ist(an.updated_at)
                             task_objs.append(task_obj)
                         task_objs.sort(key=lambda x: x["id"])
                         final_dict = {}
@@ -389,6 +391,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                             tas = tas.values()[0]
                             tas["annotation_status"] = task_obj["annotation_status"]
                             tas["user_mail"] = task_obj["user_mail"]
+                            if("unlabeled" not in ann_status):
+                                task_obj["updated_at"] = utc_to_ist(an.updated_at)
                             ordered_tasks.append(tas)
                         if page_number is not None:
                             page_object = Paginator(ordered_tasks, records)
@@ -450,6 +454,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                     task_obj["annotation_status"] = an.annotation_status
                     task_obj["user_mail"] = an.completed_by.email
                     task_obj["annotation_result_json"] = an.result
+                    if("unlabeled" not in ann_status):
+                        task_obj["updated_at"] = utc_to_ist(an.updated_at)
                     task_objs.append(task_obj)
                 task_objs.sort(key=lambda x: x["id"])
                 final_dict = {}
@@ -460,6 +466,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                     tas = tas.values()[0]
                     tas["annotation_status"] = task_obj["annotation_status"]
                     tas["user_mail"] = task_obj["user_mail"]
+                    if("unlabeled" not in ann_status):
+                        tas["updated_at"] = task_obj["updated_at"]
                     if (ann_status[0] in ["labeled", "draft", "to_be_revised"]) and (
                         proj_type == "ContextualTranslationEditing"
                     ):
@@ -519,6 +527,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                             task_obj["id"] = an.task_id
                             task_obj["annotation_status"] = an.annotation_status
                             task_obj["user_mail"] = an.completed_by.email
+                            if "unreviewed" not in rew_status:
+                                task_obj["updated_at"] = utc_to_ist(an.updated_at)
                             task_objs.append(task_obj)
                         task_objs.sort(key=lambda x: x["id"])
                         ordered_tasks = []
@@ -532,6 +542,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                             tas = tas.values()[0]
                             tas["review_status"] = task_obj["annotation_status"]
                             tas["user_mail"] = task_obj["user_mail"]
+                            if "unreviewed" not in rew_status:
+                                task_obj["updated_at"] = utc_to_ist(an.updated_at)
                             # if required_annotators_per_task > 1:
                             #     review_ann = [
                             #         a
@@ -622,6 +634,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                     task_obj["annotation_status"] = an.annotation_status
                     task_obj["user_mail"] = an.completed_by.email
                     task_obj["reviewer_annotation"] = an.result
+                    if "unlabeled" not in rew_status:
+                        task_obj["updated_at"] = utc_to_ist(an.updated_at)
                     task_obj["first_annotator_annotation"] = (
                         parent_annotator_object[0].result
                         if first_annotator_object
@@ -757,6 +771,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                             task_obj["id"] = an.task_id
                             task_obj["annotation_status"] = an.annotation_status
                             task_obj["user_mail"] = an.completed_by.email
+                            if "UNVALIDATED" not in supercheck_status:
+                                task_obj["updated_at"] = utc_to_ist(an.updated_at)
                             task_objs.append(task_obj)
                         task_objs.sort(key=lambda x: x["id"])
                         ordered_tasks = []
@@ -766,6 +782,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                             tas = tas.values()[0]
                             tas["supercheck_status"] = task_obj["annotation_status"]
                             tas["user_mail"] = task_obj["user_mail"]
+                            if "UNVALIDATED" not in supercheck_status:
+                                tas["updated_at"] = task_obj["updated_at"]
                             ordered_tasks.append(tas)
 
                         if page_number is not None:
@@ -817,6 +835,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                     task_obj["annotation_status"] = an.annotation_status
                     task_obj["user_mail"] = an.completed_by.email
                     task_obj["superchecker_annotation"] = an.result
+                    if "UNVALIDATED" not in supercheck_status:
+                        task_obj["updated_at"] = utc_to_ist(an.updated_at)
                     task_obj["reviewer_mail"] = (
                         reviewer_object[0].completed_by.email
                         if reviewer_object
@@ -840,6 +860,8 @@ class TaskViewSet(viewsets.ModelViewSet, mixins.ListModelMixin):
                     tas = Task.objects.filter(id=task_obj["id"])
                     tas = tas.values()[0]
                     tas["supercheck_status"] = task_obj["annotation_status"]
+                    if "UNVALIDATED" not in supercheck_status:
+                        tas["updated_at"] = task_obj["updated_at"]
                     tas["user_mail"] = task_obj["user_mail"]
                     tas["reviewer_mail"] = task_obj["reviewer_mail"]
                     tas["annotator_mail"] = task_obj["annotator_mail"]
