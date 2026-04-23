2ba855d63517936f0c07f618ae58588a7ff3d913
sumitbhukal0007 <sumitbhukal007@gmail.com>
2026-04-23 16:28:08 +0530

    sample dataset download btn integrated in Dataset Setting

--- backend/dataset/views.py
+++ backend/dataset/views.py
@@ -341,7 +341,7 @@ class DatasetInstanceViewSet(viewsets.ModelViewSet):
             name="Download Sample Dataset in CSV format")
     def download_sample_dataset(self, request, pk):
         """
-        View to download a sample dataset in CSV format (temporary can add others later)
+        View to download a sample dataset in CSV format (one format supported, can add others later)
         URL: /data/instances/<instance-id>/download/sampledataset
         Accepted methods: GET
         """
@@ -354,24 +354,24 @@ class DatasetInstanceViewSet(viewsets.ModelViewSet):
         dataset_model = apps.get_model("dataset", dataset_instance.dataset_type)
         dataset_resource = resources.RESOURCE_MAP[dataset_instance.dataset_type]
         data_item = dataset_model.objects.filter(instance_id=pk).first()
-        if not data_item:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        dataset = dataset_resource().export([data_item])
+        
+        # Export one sample row if available, otherwise export an empty dataset (headers only)
+        dataset = dataset_resource().export([data_item] if data_item else [])
         
         # These are typically fields that are auto-generated. Add here for exculding as mandatory
         excluded_headers = [
-            "id", 
+            "id",
             "instance_id",
             "prompt_output_pair_id", "instruction_id", "no_of_turns", "time_taken",
             "datetime", "meta_info_structure", "meta_info_language",
             "interaction_id", "eval_form_output_json", "eval_time_taken",
             "parent_interaction_ids", "multiple_interaction_json", "eval_form_json", "no_of_models",
-            ]
+        ]
 
-        # These are field which mostly have really big text length. Add here for shortening
+        # These are fields which mostly have really big text length. Add here for shortening
         shorten_fields = ["instruction_data", "interactions_json", "output", "eval_form_json", "multiple_interaction_json"]
 
-        if len(dataset) > 0:
+        if data_item and len(dataset) > 0:
             row = list(dataset[0])
             for i, header in enumerate(dataset.headers):
                 if header in excluded_headers:
@@ -412,6 +412,11 @@ class DatasetInstanceViewSet(viewsets.ModelViewSet):
         instructions = ["Fields instance_id & id are auto-generated and should be left blank."] + [''] * (len(dataset.headers) - 1)
         dataset.insert(2, field_instructions)
         dataset.insert(3, instructions)
+        
+        # If the dataset has no data items, insert an empty-dataset notice in row 5
+        if not data_item:
+            empty_notice = ["This dataset is empty."] + [''] * (len(dataset.headers) - 1)
+            dataset.insert(5, empty_notice)
         
         # Prepare StreamingHttpResponse
         response = StreamingHttpResponse(dataset.csv, content_type="text/csv")
